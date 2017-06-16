334928777e64f14a1db3db69fb1616c966e18cd1
Cory Wheeler <corywheeler@gmail.com>
2017-06-16 05:25:24 -0400

    WIP - This is working.
    There was a compile time message that System.Runtime was needed for the
    CalculationTests class that the System.Runtime dll was needed. In all honesty
    I was quite lost for some time about where/what that was and where it came
    from. There were several red herrings that were very confusing.
    
    In the end, it's a package in nuget that needs to be pulled in, named
    System.Runtime. I swear I had looked for it before, but alas... there it is.

---
+++ RefactoringToPatterns.Tests/ReplaceConstructorsWithCreationMethods/CapitalCalculationTests.cs
@@ -0,0 +1,25 @@
+﻿﻿using NUnit.Framework;
+using System;
+using ReplaceConstructorsWithCreationMethods;
+
+namespace RefactoringToPatterns.Tests.ReplaceConstructorsWithCreationMethods
+{
+    [TestFixture]
+    public class CapitalCalculationTests
+    {
+		private const double commitment = 3.0;
+		private const int riskRating = 5;
+        DateTime maturity = DateTime.MinValue;
+
+        [SetUp]
+		public void Init()
+		{
+		}
+
+        [Test]
+        public void GiveMeAGoodName()
+        {
+            Assert.IsInstanceOf(typeof(CapitalStrategyRevolver), new Loan(commitment, riskRating, maturity));
+        }
+    }
+}
