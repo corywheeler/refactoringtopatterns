200b2a20b2d06ca33b852a87dffa06f25a1f8efd
Cory Wheeler <corywheeler@gmail.com>
2017-06-16 19:37:36 -0400

    make some adjustments to make DateTime nullable to fall in line with the way it is described in the book.

--- ReplaceConstructorsWithCreationMethods/InitialCode/Loan.cs
+++ ReplaceConstructorsWithCreationMethods/InitialCode/Loan.cs
@@ -5,43 +5,41 @@ namespace ReplaceConstructorsWithCreationMethods.InitialCode
 	public class Loan
 	{
 		private double _commitment;
-		readonly double _outstanding;
-		readonly int _riskRating;
-		readonly DateTime _maturity;
-		readonly DateTime _expiry;
-		readonly CapitalStrategy _capitalStrategy;
+		private readonly double _outstanding;
+		private readonly int _riskRating;
+		private readonly DateTime? _maturity;
+		private readonly DateTime? _expiry;
+		private readonly CapitalStrategy _capitalStrategy;
 
-		public Loan(double commitment, int riskRating, DateTime maturity)
-			: this(commitment, 0.00, riskRating, maturity, DateTime.MinValue)
+		public Loan(double commitment, int riskRating, DateTime? maturity)
+            : this(commitment, 0.00, riskRating, maturity, null)
 		{
 
 		}
 
-		public Loan(double commitment, int riskRating,
-					DateTime maturity, DateTime expiry)
+		public Loan(double commitment, int riskRating, DateTime? maturity, DateTime? expiry)
 			: this(commitment, 0.00, riskRating, maturity, expiry)
 		{
 
 		}
 
-		public Loan(double commitment, double outstanding, int riskRating,
-					DateTime maturity, DateTime expiry)
+		public Loan(double commitment, double outstanding, 
+                    int riskRating, DateTime? maturity, DateTime? expiry)
 			: this(null, commitment, outstanding, riskRating, maturity, expiry)
 		{
 
 		}
 
 		public Loan(CapitalStrategy capitalStrategy, double commitment,
-					int riskRating, DateTime maturity, DateTime expiry)
-			: this(capitalStrategy, commitment,
-				   0.00, riskRating, maturity, expiry)
+					int riskRating, DateTime? maturity, DateTime? expiry)
+			: this(capitalStrategy, commitment, 0.00, riskRating, maturity, expiry)
 		{
 
 		}
 
 		public Loan(CapitalStrategy capitalStrategy, double commitment,
-					double outstanding, int riskRating, DateTime maturity,
-					DateTime expiry)
+					double outstanding, int riskRating,
+                    DateTime? maturity, DateTime? expiry)
 		{
 			this._commitment = commitment;
 			this._outstanding = outstanding;
@@ -52,9 +50,9 @@ namespace ReplaceConstructorsWithCreationMethods.InitialCode
 
 			if (capitalStrategy == null)
 			{
-				if (expiry == DateTime.MinValue)
+				if (expiry == null)
 					this._capitalStrategy = new CapitalStrategyTermLoan();
-				else if (maturity == DateTime.MinValue)
+				else if (maturity == null)
 					this._capitalStrategy = new CapitalStrategyRevolver();
 				else
 					this._capitalStrategy = new CapitalStrategyRCTL();
