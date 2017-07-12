3e01f7cf33b3b40673b9fe02d410762405052339
Cory Wheeler <corywheeler@gmail.com>
2017-07-12 13:10:50 -0400

    Remove duplication between the first and third constructors

--- ChainConstructors/MyWork/Loan.cs
+++ ChainConstructors/MyWork/Loan.cs
@@ -9,15 +9,11 @@ namespace ChainConstructors.MyWork
         private float _outstanding;
         private int _rating;
         private DateTime _expiry;
-        private DateTime _maturity;
+        private DateTime? _maturity;
 
-        public Loan(float notional, float outstanding, int rating, DateTime expiry)
+        public Loan(float notional, float outstanding, int rating, DateTime expiry) :this(new TermROC(), notional, outstanding, rating, expiry, null)
         {
-			this._strategy = new TermROC();
-			this._notional = notional;
-			this._outstanding = outstanding;
-			this._rating = rating;
-            this._expiry = expiry;
+
         }
 
         public Loan(float notional, float outstanding, int rating, DateTime expiry, DateTime maturity) 
@@ -31,7 +27,7 @@ namespace ChainConstructors.MyWork
         }
 
         public Loan(CapitalStrategy strategy, float notional, float outstanding, 
-                    int rating, DateTime expiry, DateTime maturity)
+                    int rating, DateTime expiry, DateTime? maturity)
         {
             this._strategy = strategy;
             this._notional = notional;
