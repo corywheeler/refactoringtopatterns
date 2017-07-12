09c2d5a68594c5174623f4f0b6deebd4238cf67b
Erin Stanfill <erin.stanfill@versionone.com>
2017-07-12 13:02:35 -0400

    chain constructors refactoring

--- ChainConstructors/MyWork/Loan.cs
+++ ChainConstructors/MyWork/Loan.cs
@@ -8,30 +8,20 @@ namespace ChainConstructors.MyWork
         private float _notional;
         private float _outstanding;
         private int _rating;
-        private DateTime _expiry;
-        private DateTime _maturity;
+        private DateTime? _expiry;
+        private DateTime? _maturity;
 
-        public Loan(float notional, float outstanding, int rating, DateTime expiry)
-        {
-			this._strategy = new TermROC();
-			this._notional = notional;
-			this._outstanding = outstanding;
-			this._rating = rating;
-            this._expiry = expiry;
-        }
+        public Loan(float notional, float outstanding, int rating, DateTime? expiry):
+	        this(new TermROC(), notional, outstanding, rating, expiry, null)
+		{ }
 
-        public Loan(float notional, float outstanding, int rating, DateTime expiry, DateTime maturity) 
+        public Loan(float notional, float outstanding, int rating, DateTime? expiry, DateTime? maturity):
+			this(new RevolvingTermROC(), notional,outstanding,rating,expiry,maturity) 
         {
-            this._strategy = new RevolvingTermROC();
-            this._notional = notional;
-            this._outstanding = outstanding;
-            this._rating = rating;
-            this._expiry = expiry;
-            this._maturity = maturity;
         }
 
         public Loan(CapitalStrategy strategy, float notional, float outstanding, 
-                    int rating, DateTime expiry, DateTime maturity)
+                    int rating, DateTime? expiry, DateTime? maturity)
         {
             this._strategy = strategy;
             this._notional = notional;
