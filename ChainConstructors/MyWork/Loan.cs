5138d6ac643cc2fad05823fdd4e20f0efda01327
Cory Wheeler <corywheeler@gmail.com>
2017-07-12 13:12:37 -0400

    Remove duplication of constructor code between second and third constructor

--- ChainConstructors/MyWork/Loan.cs
+++ ChainConstructors/MyWork/Loan.cs
@@ -16,14 +16,9 @@ namespace ChainConstructors.MyWork
 
         }
 
-        public Loan(float notional, float outstanding, int rating, DateTime expiry, DateTime maturity) 
+        public Loan(float notional, float outstanding, int rating, DateTime expiry, DateTime maturity) : this(new RevolvingTermROC(), notional,outstanding, rating, expiry, maturity)
         {
-            this._strategy = new RevolvingTermROC();
-            this._notional = notional;
-            this._outstanding = outstanding;
-            this._rating = rating;
-            this._expiry = expiry;
-            this._maturity = maturity;
+
         }
 
         public Loan(CapitalStrategy strategy, float notional, float outstanding, 
