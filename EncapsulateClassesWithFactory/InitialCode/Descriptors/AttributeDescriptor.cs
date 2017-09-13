2ca5d00229894db9440afa64c0765517193e7846
Cory Wheeler <corywheeler@gmail.com>
2017-09-12 21:52:38 -0400

    WIP: Initial set of code for EncapsulateClassesWithFactory refactoring
    Encapsulate Classes With Factory is found on p.80 of Refatoring To Patterns
    This is the initial set of code with tests excercising the code.
    
    You'll notice that the tests use a testing subclass to gain access to
    the AttributeDescriptors. A good review of usages of testing subclasses
    can be found at:
    
    http://xunitpatterns.com/Test-Specific%20Subclass.html

---
+++ EncapsulateClassesWithFactory/InitialCode/Descriptors/AttributeDescriptor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EncapsulateClassesWithFactory.InitialCode.Descriptors
+{
+    public class AttributeDescriptor
+    {
+        readonly string descriptorName;
+        readonly Type mapperType;
+        readonly Type forType;
+
+        protected AttributeDescriptor(string descriptorName, Type mapperType, Type forType)
+        {
+            this.descriptorName = descriptorName;
+            this.mapperType = mapperType;
+            this.forType = forType;
+        }
+
+        public string DescriptorName => descriptorName;
+    }
+}
---
+++ RefactoringToPatterns.Tests/EncapsulateClassesWithFactory/InitialCode/Mappers/DescriptorMapperTests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using EncapsulateClassesWithFactory.InitialCode.Descriptors;
+using EncapsulateClassesWithFactory.InitialCode.Mappers;
+using System.Collections.Generic;
+
+namespace RefactoringToPatterns.EncapsulateClassesWithFactory.InitialCode.Mappers
+{
+    [TestFixture]
+    public class DescriptorMapperTests
+    {
+        TestingDescriptorMapper testDescriptorMapper;
+
+		[SetUp]
+        public void Init()
+        {
+            testDescriptorMapper = new TestingDescriptorMapper();
+        }
+
+        [Test]
+        public void it_maps_remoteId_descriptor()
+        {
+            var remoteIdDescriptor = 
+                testDescriptorMapper.GetMappedDescriptorFor("remoteId");
+            
+            Assert.NotNull(remoteIdDescriptor);
+        }
+
+        [Test]
+		public void it_maps_createdDate_descriptor()
+		{
+			var createdDateDescriptor = 
+                testDescriptorMapper.GetMappedDescriptorFor("createdDate");
+            
+			Assert.NotNull(createdDateDescriptor);
+		}
+
+		[Test]
+		public void it_maps_lastChangedDate_descriptor()
+		{
+			var lastChangedDateDescriptor = 
+                testDescriptorMapper.GetMappedDescriptorFor("lastChangedDate");
+            
+			Assert.NotNull(lastChangedDateDescriptor);
+		}
+
+		[Test]
+		public void it_maps_createdBy_descriptor()
+		{
+            var createdByDescriptor = 
+                testDescriptorMapper.GetMappedDescriptorFor("createdBy");
+            
+			Assert.NotNull(createdByDescriptor);
+		}
+
+		[Test]
+		public void it_maps_lastChangedBy_descriptor()
+		{
+			var lastChangedByDescriptor = 
+                testDescriptorMapper.GetMappedDescriptorFor("lastChangedBy");
+            
+			Assert.NotNull(lastChangedByDescriptor);
+		}
+
+		[Test]
+		public void it_maps_optimisticLockVersion_descriptor()
+		{
+			var optimisticLockVersionDescriptor = 
+                testDescriptorMapper.GetMappedDescriptorFor("optimisticLockVersion");
+            
+			Assert.NotNull(optimisticLockVersionDescriptor);
+		}
+
+		[Test]
+		public void it_does_not_map_unknown_descriptors()
+		{
+            var unknownDescriptor = 
+                testDescriptorMapper.GetMappedDescriptorFor("unknown");
+            
+			Assert.Null(unknownDescriptor);
+		}
+    }
+
+    internal class TestingDescriptorMapper : DescriptorMapper {
+        List<AttributeDescriptor> descriptors;
+
+        public TestingDescriptorMapper() {
+            descriptors = CreateAttributeDescriptors();
+        }
+
+		public AttributeDescriptor GetMappedDescriptorFor(string descriptorName)
+		{
+			return descriptors.Find(descriptor => descriptor.DescriptorName == descriptorName);
+		}
+    }
+}
+
