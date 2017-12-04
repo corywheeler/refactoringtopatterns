02c61a83ad55fe08164ec2598ca329c807ceb5fa
Cory Wheeler <corywheeler@gmail.com>
2017-12-03 22:58:06 -0500

    Set up the DomBuilderTest class

---
+++ PolymorphicCreationWithFactoryMethod/InitialCode/DomBuilderTest.cs
@@ -0,0 +1,39 @@
+﻿namespace PolymorphicCreationWithFactoryMethod.InitialCode
+{
+    public class DomBuilderTest : TestCase
+    {
+        private OutputBuilder builder;
+
+        public OutputBuilder Builder { get => builder; private set => builder = value; }
+
+        public void TestAddAboveRoot()
+        {
+            string invalidResult =
+                "<orders>" +
+                    "<order>" +
+                    "</order>" +
+                "</orders>" +
+                "<customer>" +
+                "</customer>";
+
+            builder = new DOMBuilder();
+
+            builder.AddBelow("order");
+
+            try
+            {
+                builder.AddAbove("customer");
+                Fail("expecting PolymorphicCreationWithFactoryMethod RuntimeException");
+            }
+            catch (RuntimeException ignored)
+            {
+
+            }
+        }
+
+        private void Fail(string failureMessage)
+        {
+
+        }
+    }
+}
