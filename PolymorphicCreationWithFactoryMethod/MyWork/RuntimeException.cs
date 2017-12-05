327a1c25eb4b0e46d238a06dde78efb1d233ea52
Cory Wheeler <corywheeler@gmail.com>
2017-12-04 23:32:24 -0500

    Set up the MyWork workspace for personal refactoring space

---
+++ PolymorphicCreationWithFactoryMethod/MyWork/RuntimeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PolymorphicCreationWithFactoryMethod.MyWork
+{
+    [Serializable]
+    public class RuntimeException : Exception
+    {
+        public RuntimeException()
+        {
+        }
+
+        public RuntimeException(string message) : base(message)
+        {
+        }
+
+        public RuntimeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected RuntimeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ PolymorphicCreationWithFactoryMethod/MyWork/XMLBuilderTest.cs
@@ -0,0 +1,37 @@
+﻿namespace PolymorphicCreationWithFactoryMethod.MyWork
+{
+    public class XMLBuilderTest: TestCase
+    {
+        public OutputBuilder Builder { get; private set; }
+
+        public void TestAddAboveRoot()
+		{
+			string invalidResult =
+				"<orders>" +
+					"<order>" +
+					"</order>" +
+				"</orders>" +
+				"<customer>" +
+				"</customer>";
+
+			Builder = new XMLBuilder();
+
+			Builder.AddBelow("order");
+
+			try
+			{
+				Builder.AddAbove("customer");
+				Fail("expecting RuntimeException");
+			}
+			catch (RuntimeException ignored)
+			{
+
+			}
+		}
+
+		private void Fail(string failureMessage)
+		{
+
+		}
+    }
+}
