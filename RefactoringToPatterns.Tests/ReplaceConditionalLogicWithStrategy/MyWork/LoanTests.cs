d4ccf37e6cf4fc89e57d97a0fe175f81b07c5581
Cory Wheeler <corywheeler@gmail.com>
2018-01-28 19:15:19 -0500

    Make the MyWork sections.

---
+++ RefactoringToPatterns.Tests/ReplaceConditionalLogicWithStrategy/MyWork/LoanTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using NUnit.Framework;
+using ReplaceConditionalLogicWithStrategy.MyWork;
+
+namespace RefactoringToPatterns.ReplaceConditionalLogicWithStrategy.MyWork
+{
+    [TestFixture()]
+    public class LoanTests
+    {
+        private readonly int LOW_RISK_TAKING = 2;
+        private readonly int HIGH_RISK_TAKING = 5;
+        private readonly double LOAN_AMOUNT = 10000.00;
+        private readonly double TWO_DIGIT_PRECISION = 0.001;
+
+        [SetUp]
+        public void Init() {
+
+        }
+
+        [Test()]
+        public void test_term_loan_same_payments()
+        {
+            DateTime start = November(20, 2003);
+            DateTime maturity = November(20, 2006);
+
+			Loan termLoan = Loan.NewTermLoan(LOAN_AMOUNT, start, maturity, HIGH_RISK_TAKING);
+            termLoan.Payment(1000.00, November(20, 2004));
+            termLoan.Payment(1000.00, November(20, 2005));
+            termLoan.Payment(1000.00, November(20, 2006));
+
+            Assert.Multiple(() => {
+                Assert.AreEqual(20027, termLoan.Duration(), TWO_DIGIT_PRECISION);
+                Assert.AreEqual(6008100, termLoan.Capital(), TWO_DIGIT_PRECISION);
+            });
+        }
+
+        [Test()]
+        public void test_revolver_loan_same_payments()
+        {
+            DateTime start = November(20, 2003);
+            DateTime expiry = November(20, 2007);
+
+            Loan termLoan = Loan.NewRevolver(LOAN_AMOUNT, start, expiry, HIGH_RISK_TAKING);
+            termLoan.Payment(1000.00, November(20, 2004));
+            termLoan.Payment(1000.00, November(20, 2005));
+            termLoan.Payment(1000.00, November(20, 2006));
+
+            Assert.Multiple(() => {
+                Assert.AreEqual(40027, termLoan.Duration(), TWO_DIGIT_PRECISION);
+                Assert.AreEqual(4002700, termLoan.Capital(), TWO_DIGIT_PRECISION);
+            });
+        }
+
+        [Test()]
+        public void test_advised_line_loan_same_payments()
+        {
+            DateTime start = November(20, 2003);
+            DateTime maturity = November(20, 2006);
+            DateTime expiry = November(20, 2007);
+
+            Loan termLoan = Loan.NewAdvisedLine(LOAN_AMOUNT, start, expiry, LOW_RISK_TAKING);
+            termLoan.Payment(1000.00, November(20, 2004));
+            termLoan.Payment(1000.00, November(20, 2005));
+            termLoan.Payment(1000.00, November(20, 2006));
+
+            Assert.Multiple(() => {
+                Assert.AreEqual(40027, termLoan.Duration(), TWO_DIGIT_PRECISION);
+                Assert.AreEqual(1200810, termLoan.Capital(), TWO_DIGIT_PRECISION);
+            });
+        }
+
+        private static DateTime November(int dayOfMonth, int year)
+        {
+            return new DateTime(year, 11, dayOfMonth);
+        }
+    }
+}
---
+++ ReplaceConditionalLogicWithStrategy/MyWork/Loan.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReplaceConditionalLogicWithStrategy.MyWork
+{
+    public class Loan
+    {
+        double _commitment = 1.0;
+        private DateTime? _expiry;
+        private DateTime? _maturity;
+        private double _outstanding;
+        IList<Payment> _payments = new List<Payment>();
+        private DateTime? _today = DateTime.Now;
+        private DateTime _start;
+        private long MILLIS_PER_DAY = 86400000;
+        private long DAYS_PER_YEAR = 365;
+        private double _riskRating;
+        private double _unusedPercentage;
+
+        public Loan(double commitment, double notSureWhatThisIs, DateTime start, DateTime? expiry, DateTime? maturity, int riskRating)
+        {
+            this._expiry = expiry;
+            this._commitment = commitment;
+            this._today = null;
+            this._start = start;
+            this._maturity = maturity;
+            this._riskRating = riskRating;
+            this._unusedPercentage = 1.0;
+        }
+
+        public static Loan NewTermLoan(double commitment, DateTime start, DateTime maturity, int riskRating)
+        {
+            return new Loan(commitment, commitment, start, null, 
+                            maturity, riskRating);
+        }
+
+        public static Loan NewRevolver(double commitment, DateTime start, DateTime expiry, int riskRating) 
+        {
+            return new Loan(commitment, 0, start, expiry,
+                            null, riskRating);
+        }
+
+        public static Loan NewAdvisedLine(double commitment, DateTime start, DateTime expiry, int riskRating)
+        {
+            if (riskRating > 3) return null;
+            Loan advisedLine = new Loan(commitment, 0, start, expiry,
+                            null, riskRating);
+            advisedLine.SetUnusedPercentage(0.1);
+            return advisedLine;
+        }
+
+        public void Payment(double amount, DateTime paymentDate)
+        {
+            _payments.Add(new Payment(amount, paymentDate));
+        }
+
+        public double Capital() {
+            if(_expiry == null && _maturity != null)
+                return _commitment * Duration() * RiskFactor();
+            if(_expiry != null && _maturity == null) {
+                if(GetUnusedPercentage() != 1.0) {
+                    return _commitment * GetUnusedPercentage() * Duration() * RiskFactor();
+                }
+                else {
+                    return (OutstandingRiskAmount() * Duration() * RiskFactor())
+                        + (UnusedRiskAmount() * Duration() * UnusedRiskFactor());
+                }
+            }
+            return 0.0;
+        }
+
+        public double Duration()
+        {
+            if (_expiry == null && _maturity != null)
+            {
+                return WeightedAverageDuration();
+            }
+            else if (_expiry != null && _maturity == null)
+            {
+                return YearsTo(_expiry);
+            }
+            return 0.0;
+        }
+
+        private double WeightedAverageDuration()
+        { 
+            double duration = 0.0;
+            double weightedAverage = 0.0;
+            double sumOfPayments = 0.0;
+
+            foreach (var payment in _payments)
+            {
+                sumOfPayments += payment.Amount;
+                weightedAverage += YearsTo(payment.Date) * payment.Amount;
+            }
+
+            if (_commitment != 0.0)
+            {
+                duration = weightedAverage / sumOfPayments;
+            }
+
+            return duration;
+        }
+
+        private double YearsTo(DateTime? endDate)
+        {
+            DateTime? beginDate = (_today == null ? _start : _today);
+            return (double)((endDate?.Ticks - beginDate?.Ticks) / MILLIS_PER_DAY / DAYS_PER_YEAR);
+        }
+
+        private double RiskFactor()
+        {
+            return InitialCode.RiskFactor.GetFactors().ForRating(_riskRating);
+        }
+
+        private double GetUnusedPercentage()
+        {
+            return _unusedPercentage;
+        }
+
+        public void SetUnusedPercentage(double unusedPercentage) 
+        {
+            _unusedPercentage = unusedPercentage;
+        }
+
+        private double UnusedRiskAmount()
+        {
+            return (_commitment - _outstanding);
+        }
+
+        private double UnusedRiskFactor()
+        {
+            return UnusedRiskFactors.GetFactors().ForRating(_riskRating);
+        }
+
+        private double OutstandingRiskAmount()
+        {
+            return _outstanding;
+        }
+    }
+}
