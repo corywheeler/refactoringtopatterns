5a2cb5bc5e2255ee44ae9321125bf1ff327bdca8
Cory Wheeler <corywheeler@gmail.com>
2018-03-17 12:44:27 -0400

    FormTemplateMethod: Put copies of everything into MyWork.
    This sets up the starting point for people to experiment with
    putting in their own solution to this refactoring.

---
+++ FormTemplateMethod/MyWork/CapitalStrategyTermLoan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace FormTemplateMethod.MyWork
+{
+    public class CapitalStrategyTermLoan : CapitalStrategy
+    {
+        private readonly double EPSILON = 0.001;
+
+        public override double Capital(Loan loan)
+        {
+            return loan.GetCommitment() * Duration(loan) * RiskFactorFor(loan);
+        }
+
+        public override double Duration(Loan loan)
+        {
+            return WeightedAverageDuration(loan);
+        }
+
+        private double WeightedAverageDuration(Loan loan)
+        {
+            double duration = 0.0;
+            double weightedAverage = 0.0;
+            double sumOfPayments = 0.0;
+
+            foreach (var payment in loan.Payments())
+            {
+                sumOfPayments += payment.Amount;
+                weightedAverage += YearsTo(payment.Date, loan) * payment.Amount;
+            }
+
+            if (Math.Abs(loan.GetCommitment()) > EPSILON)
+            {
+                duration = weightedAverage / sumOfPayments;
+            }
+
+            return duration;
+        }
+    }
+}
---
+++ RefactoringToPatterns.Tests/FormTemplateMethod/MyWork/RevolverStrategyTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using FormTemplateMethod.MyWork;
+
+namespace RefactoringToPatterns.FormTemplateMethod.MyWork
+{
+    [TestFixture()]
+    public class RevolverStrategyTests
+    {
+        private readonly int HIGH_RISK_TAKING = 5;
+        private readonly double LOAN_AMOUNT = 10000.00;
+        private readonly double TWO_DIGIT_PRECISION = 0.001;
+
+        [Test()]
+        public void test_revolver_loan_same_payments()
+        {
+            var revolverStrategy = new CapitalStrategyRevolver();
+            DateTime start = November(20, 2003);
+            DateTime expiry = November(20, 2007);
+
+            Loan revolverLoan = Loan.NewRevolver(LOAN_AMOUNT, start, expiry, HIGH_RISK_TAKING);
+            revolverLoan.Payment(1000.00, November(20, 2004));
+            revolverLoan.Payment(1000.00, November(20, 2005));
+            revolverLoan.Payment(1000.00, November(20, 2006));
+
+            Assert.Multiple(() => {
+                Assert.AreEqual(40027, revolverStrategy.Duration(revolverLoan), TWO_DIGIT_PRECISION);
+                Assert.AreEqual(4002700, revolverStrategy.Capital(revolverLoan), TWO_DIGIT_PRECISION);
+            });
+        }
+
+        private static DateTime November(int dayOfMonth, int year)
+        {
+            return new DateTime(year, 11, dayOfMonth);
+        }
+    }
+}
