f90c4809b3de8b2839e1473bbd0b6f79d8d0a355
Cory Wheeler <corywheeler@gmail.com>
2019-02-11 22:51:44 -0500

    Move add to be a ComposedMethod.

--- ComposeMethod/MyWork/List.cs
+++ ComposeMethod/MyWork/List.cs
@@ -4,28 +4,46 @@ namespace ComposeMethod.MyWork
 {
     public class List
     {
+        private static int GROWTH_INCREMENT = 10;
         private bool _readOnly;
         private int _size;
         private Object[] _elements = new Object[] { };
 
         public void Add(Object element)
         {
-            if (!_readOnly)
+
+            if (_readOnly)
             {
-                int newSize = _size + 1;
-
-                if (newSize > _elements.Length)
-                {
-                    Object[] newElements = new Object[_elements.Length + 10];
-
-                    for (int i = 0; i < _size; i++)
-                        newElements[i] = _elements[i];
-
-                    _elements = newElements;
-                }
-
-                _elements[_size++] = element;
+                return;
             }
+
+            if (AtCapacity())
+            {
+                Grow();
+            }
+
+            AddElement(element);
+
+        }
+
+        private bool AtCapacity()
+        {
+            return _size + 1 > _elements.Length;
+        }
+
+        private void Grow()
+        {
+            Object[] newElements = new Object[_elements.Length + GROWTH_INCREMENT];
+
+            for (int i = 0; i < _size; i++)
+                newElements[i] = _elements[i];
+
+            _elements = newElements;
+        }
+
+        private void AddElement(object element)
+        {
+            _elements[_size++] = element;
         }
 
         public int Count
