80fa5c278fbec5b03af6991fcffad2726623236d
Cory Wheeler <corywheeler@gmail.com>
2019-02-19 22:23:11 -0500

    Introduction of a TagNode that can be used for implicit leaf nodes
    The TagNode class can be used to represent all implicit leaves in the
    implicit tree.

---
+++ RefactoringToPatterns.Tests/ReplaceImplicitTreeWithComposite/MyWork/TagTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using ReplaceImplicitTreeWithComposite.MyWork;
+
+namespace RefactoringToPatterns.ReplaceImplicitTreeWithComposite.MyWork
+{
+    [TestFixture]
+    public class TagTests
+    {
+        private const string SamplePrice = "2.39";
+
+        [Test]
+        public void TestSimpleTagWithOneAttributeAndValue()
+        {
+            var expected =
+                "<price currency=" +
+                "'" +
+                "USD" +
+                "'>" +
+                SamplePrice +
+                "</price>";
+
+            TagNode priceTag = new TagNode("price");
+            priceTag.AddAttribute("currency", "USD");
+            priceTag.AddValue(SamplePrice);
+            Assert.AreEqual(expected, priceTag.ToString());
+        }
+    }
+}
---
+++ ReplaceImplicitTreeWithComposite/MyWork/TagNode.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace ReplaceImplicitTreeWithComposite.MyWork
+{
+    public class TagNode
+    {
+        private string name;
+        private StringBuilder attributes;
+        private string value = string.Empty;
+
+        public TagNode(string name)
+        {
+            this.name = name;
+            this.attributes = new StringBuilder();
+        }
+
+        public void AddAttribute(string attribute, string value)
+        {
+            this.attributes.Append(" ");
+            this.attributes.Append(attribute);
+            this.attributes.Append("='");
+            this.attributes.Append(value);
+            this.attributes.Append("'");
+        }
+
+        public void AddValue(string value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            var result =
+                "<" + this.name + this.attributes + ">" +
+                this.value +
+                "</" + this.name + ">";
+            return result;
+        }
+    }
+}
