3394d9673c2144c2d839853b84043ef5f00be651
Cory Wheeler <corywheeler@gmail.com>
2019-02-20 21:48:10 -0500

    Initial and MyWork Code for EncapsulateCompositeWithBuilder refactoring

---
+++ RefactoringToPatterns.Tests/EncapsulateCompositeWithBuilder/InitialCode/TagTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using EncapsulateCompositeWithBuilder.InitialCode;
+
+namespace RefactoringToPatterns.EncapsulateCompositeWithBuilder.InitialCode
+{
+    [TestFixture]
+    public class TagTests
+    {
+        private const string SamplePrice = "2.39";
+
+        [Test]
+        public void TestSimpleTagWithOneAttributeAndValue()
+        {
+            var expected =
+                "<price currency=" +
+                "'" +
+                "USD" +
+                "'>" +
+                SamplePrice +
+                "</price>";
+
+            TagNode priceTag = new TagNode("price");
+            priceTag.AddAttribute("currency", "USD");
+            priceTag.AddValue(SamplePrice);
+            Assert.AreEqual(expected, priceTag.ToString());
+        }
+
+        [Test]
+        public void TestCompositeTagoneChild()
+        {
+            var expected =
+                "<product>" +
+                    "<price>" +
+                    "</price>" +
+                "</product>"; 
+
+            TagNode productTag = new TagNode("product");
+            productTag.Add(new TagNode("price"));
+
+            Assert.AreEqual(expected, productTag.ToString());
+        }
+
+        [Test]
+        public void TestAddingChildrenAndGrandChildren()
+        {
+            var expected =
+                "<orders>" +
+                    "<order>" +
+                        "<product>" +
+                        "</product>" +
+                    "</order>" +
+                "</orders>";
+
+            TagNode ordersTag = new TagNode("orders");
+            TagNode orderTag = new TagNode("order");
+            orderTag.Add(new TagNode("product"));
+            ordersTag.Add(orderTag);
+
+            Assert.AreEqual(expected, ordersTag.ToString());
+        }
+    }
+}
