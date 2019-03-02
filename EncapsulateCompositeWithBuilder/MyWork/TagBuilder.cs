df2122005aad0393870233e6156e932308a8f94d
Cory Wheeler <corywheeler@gmail.com>
2019-03-01 20:31:18 -0500

    A few additional tests to further excercise TagBuilder

--- EncapsulateCompositeWithBuilder/MyWork/TagBuilder.cs
+++ EncapsulateCompositeWithBuilder/MyWork/TagBuilder.cs
@@ -1,19 +1,34 @@
-﻿using EncapsulateCompositeWithBuilder.MyWork;
+﻿using System;
+using EncapsulateCompositeWithBuilder.MyWork;
 
 namespace EncapsulateCompositeWithBuilder.MyWork
 {
     public class TagBuilder
     {
-        private TagNode rootTagName;
+        private TagNode rootNode;
+        private TagNode currentNode;
 
         public TagBuilder(string rootTagName)
         {
-            this.rootTagName = new TagNode(rootTagName);
+            this.rootNode = new TagNode(rootTagName);
+            this.currentNode = this.rootNode;
         }
 
         public string ToXml()
         {
-            return this.rootTagName.ToString();
+            return this.rootNode.ToString();
+        }
+
+        public void AddChild(string childTagName)
+        {
+            TagNode parentNode = this.currentNode;
+            this.currentNode = new TagNode(childTagName);
+            parentNode.Add(this.currentNode);
+        }
+
+        public void AddSibling(string v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- RefactoringToPatterns.Tests/EncapsulateCompositeWithBuilder/MyWork/TagBuilderTest.cs
+++ RefactoringToPatterns.Tests/EncapsulateCompositeWithBuilder/MyWork/TagBuilderTest.cs
@@ -15,5 +15,63 @@ namespace RefactoringToPatterns.EncapsulateCompositeWithBuilder.MyWork
 
             Assert.AreEqual(expectedXml, actualXml);
         }
+
+        [Test]
+        public void TestBuildOneChild()
+        {
+            string expectedXml =
+                "<flavors>" +
+                  "<flavor/>" +
+                "</flavors>";
+
+            TagBuilder builder = new TagBuilder("flavors");
+            builder.AddChild("flavor");
+            string actualXml = builder.ToXml();
+
+            Assert.AreEqual(expectedXml, actualXml);
+        }
+
+        [Test]
+        public void TestBuildChildrenOfChildren()
+        {
+            string expectedXml =
+                "<flavors>" +
+                  "<flavor>" +
+                    "<requirements>" +
+                      "<requirement/>" +
+                    "</requirements>" +
+                  "</flavor>" +
+                "</flavors>";
+
+            TagBuilder builder = new TagBuilder("flavors");
+            builder.AddChild("flavor");
+            builder.AddChild("requirements");
+            builder.AddChild("requirement");
+
+            string actualXml = builder.ToXml();
+
+            Assert.AreEqual(expectedXml, actualXml);
+        }
+
+        [Test]
+        [Ignore("Restore this once corrections have been made to TagTest")]
+        public void TestBuildSibling()
+        {
+            string expectedXml =
+                "<flavors>" +
+                  "<flavor1/>" +
+                  "<flavor2/>" +
+                "</flavors>";
+
+            TagBuilder builder = new TagBuilder("flavors");
+            builder.AddChild("flavor1");
+            builder.AddSibling("flavor2");
+
+            string actualXml = builder.ToXml();
+
+            Assert.AreEqual(expectedXml, actualXml);
+
+
+        }
     }
 }
