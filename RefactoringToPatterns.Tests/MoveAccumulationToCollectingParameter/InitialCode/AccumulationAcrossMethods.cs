95c75fbd8a4c7b0afd56c47e1dec971f6295577d
Cory Wheeler <corywheeler@gmail.com>
2020-05-30 17:47:23 -0400

    Strings can't be used as accumulators across method calls.
    A small example to demonstrate how accumulation accross method calls
    does not work with the String type, similar to Java.

---
+++ RefactoringToPatterns.Tests/MoveAccumulationToCollectingParameter/InitialCode/AccumulationAcrossMethods.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Text;
+using NUnit.Framework;
+
+/*
+ * On page 315 Joshua makes the statement that Java's String would not allow you to accumulate results across methods.
+ * I was pretty sure of what he meant, and was also pretty certain that was the case with C# as well... but I wanted to
+ * check it so I wrote this small test class to explore that.
+ * 
+ * I've left a failing test in here, Accumulate_With_String_Does_Not_Work_Across_Method_Calls. This shows that passing
+ * a String into a method, and then seemingly mutating that string inside of that method... does not mutate the original
+ * String passed in.
+ *
+ * This is not the case with the second test that uses a StringBuilder,
+ * Accumulate_With_StringBuilder_Does_Work_Accross_Method_Calls.
+ *
+ * This shows the difference in how these types are passed. You may say... of course! I wanted to leave this behind
+ * for anyone that hit that part of the book and maybe didn't have an understanding of what Joshua was talking about.
+ */
+
+namespace RefactoringToPatterns.Tests.MoveAccumulationToCollectingParameter.InitialCode
+{
+    [TestFixture]
+    public class AccumulationAcrossMethods
+    {
+
+        [Test]
+        public void Accumulate_With_String_Does_Not_Work_Across_Method_Calls()
+        {
+            string expectedFullName = "Cory Wheeler";
+            string myFullName = "Cory";
+            
+            AccumulateWithString(myFullName);
+            
+            Assert.AreEqual(expectedFullName, myFullName);
+        }
+
+        [Test]
+        public void Accumulate_With_StringBuilder_Does_Work_Accross_Method_Calls()
+        {
+            string expectedFullName = "Cory Wheeler";
+            StringBuilder myFullName = new StringBuilder("Cory");
+            
+            AccumulateWithStringBuffer(myFullName);
+            
+            Assert.AreEqual(expectedFullName, myFullName.ToString());
+        }
+
+        public void AccumulateWithString(string accumulator)
+        {
+            accumulator += " " + "Wheeler";
+            
+            Console.WriteLine(accumulator);
+        }
+
+        public void AccumulateWithStringBuffer(StringBuilder accumulator)
+        {
+            accumulator.Append(" ");
+            accumulator.Append("Wheeler");
+            
+            Console.WriteLine(accumulator);
+        }
+    }
+}
