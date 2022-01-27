665a7b978fb850a0de0801df1a0b63e89cf9a8ae
Cory Wheeler <corywheeler@gmail.com>
2022-01-26 21:39:16 -0500

    Include the Tests and .proj file

---
+++ RefactoringToPatterns.Tests/ExtractComposite/MyWork/FormTagTests.cs
@@ -0,0 +1,26 @@
+using ExtractComposite.MyWork;
+using NUnit.Framework;
+
+namespace RefactoringToPatterns.ExtractComposite.MyWork
+{
+    [TestFixture]
+    public class FormTagTests
+    {
+        private FormTag _formTag;
+        
+        [SetUp]
+        public void Init()
+        {
+            _formTag = new FormTag();
+        }
+        
+        [Test]
+        public void it_should_output_a_non_html_formatted_text_representation()
+        {
+            _formTag.AddChild(new LabelTag(new StringNode("Hi, I'm A Label.")));
+            _formTag.AddChild(new LabelTag(new StringNode("You're not the only one, I'm a Label too!")));
+            
+            Assert.AreEqual("Hi, I'm A Label.You're not the only one, I'm a Label too!",_formTag.ToPlainTextString());
+        }
+    }
+}
